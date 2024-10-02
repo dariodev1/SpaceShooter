977ad8283a0df1fe6c70c678de792d4e6330a5c3
dariodev1 <dariuszszpunar@outlook.com>
2024-10-02 22:07:00 +0200

    code refactor

---
+++ SpaceShooter/Game.cs
@@ -0,0 +1,26 @@
+﻿using Raylib_cs;
+using SpaceShooter.Model;
+
+namespace SpaceShooter
+{
+    internal class Game
+    {
+        public Game() 
+        {
+            Raylib.InitWindow(1200, 800, "Hello World");
+            Player player = new Player();
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.White);
+
+                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Yellow);
+                Raylib.DrawTexture(player.Texture2D, 300, 300, Color.Brown);
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- SpaceShooter/Program.cs
+++ SpaceShooter/Program.cs
@@ -1,20 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Raylib_cs;
-using SpaceShooter.Model;
-using SpaceShooter.Model.Abstract;
+using SpaceShooter;
 
-Raylib.InitWindow(1200, 800, "Hello World");
-Player player = new Player();
-while (!Raylib.WindowShouldClose())
-{
-    Raylib.BeginDrawing();
-    Raylib.ClearBackground(Color.White);
+Game game = new Game();
 
-    Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Yellow);
-    Raylib.DrawTexture(player.Texture2D, 300, 300, Color.Brown);
-    Raylib.EndDrawing();
-}
-
-Raylib.CloseWindow();
-Console.WriteLine("Hello, World!");
