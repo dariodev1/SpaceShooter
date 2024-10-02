df2635930061939863fbf2ae9b8407c2effd5aa1
DESKTOP-TF35PDU\Dariusz <dariusz.szpunar@geotronics.com.pl>
2024-10-02 15:11:48 +0200

    init commit

---
+++ SpaceShooter/Model/Player.cs
@@ -0,0 +1,24 @@
+﻿using Raylib_cs;
+using System.Reflection;
+
+namespace SpaceShooter.Model
+{
+    internal class Player
+    {
+        public Texture2D Texture2D { get; }
+
+        public Player()
+        {
+            string startupPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName)+ @"\Assets\player.png";
+            if (File.Exists(startupPath))
+            {
+                Texture2D = Raylib.LoadTexture(startupPath);
+            }
+            else 
+            {
+                Console.WriteLine("Not Exist");
+            }
+            
+        }
+    }
+}
--- SpaceShooter/Program.cs
+++ SpaceShooter/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Raylib_cs;
+using SpaceShooter.Model;
+
+Raylib.InitWindow(3640, 2200, "Hello World");
+Player player = new Player();
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.White);
+
+    Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+    Raylib.DrawTexture(player.Texture2D, 300, 300, Color.Black);
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
 Console.WriteLine("Hello, World!");
